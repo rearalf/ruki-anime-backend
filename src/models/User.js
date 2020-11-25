6d8f34cd86d004b4dcf3b713f05b20aadc31358e
rearalf <rear_recinos@hotmail.com>
2020-11-24 19:38:41 -0600

    Modelo para usuario creado

---
+++ src/models/User.js
@@ -0,0 +1,20 @@
+const { Schema, model } = require('mongoose');
+const jwt = require('jsonwebtoken');
+
+const UserSchema = new Schema(
+	{
+		id_user: { type: String, required: true, unique: true },
+		email: { type: String, required: true, unique: true },
+		date: { type: Date, default: Date.now },
+	},
+	{
+		timestamps: true,
+	},
+);
+
+UserSchema.methods.generateAuthToken = async email => {
+	const token = await jwt.sign({ email }, process.env.JWT_KEY);
+	return token;
+};
+
+module.exports = model('User', UserSchema);
