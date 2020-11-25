b468046a4d6c0f38d6b5c1cffeba585c4703d444
rearalf <rear_recinos@hotmail.com>
2020-11-24 20:48:20 -0600

    Modelo de animes agregado

---
+++ src/models/Anime.js
@@ -0,0 +1,25 @@
+const { Schema, model } = require('mongoose');
+
+const GenresSchema = new Schema({
+	mal_id: { type: Number, required: true },
+	type: { type: String, required: true },
+	name: { type: String, required: true },
+});
+
+const AnimeSchema = new Schema(
+	{
+		id_user: { type: String, required: true, unique: true },
+		mal_id: { type: Number, required: true, unique: true },
+		image_url: { type: String, required: true },
+		title: { type: String, required: true },
+		type: { type: String, required: true },
+		genres: { type: [ GenresSchema ], required: true },
+		favorite: { type: Boolean, default: false },
+		date: { type: Date, default: Date.now },
+	},
+	{
+		timestamps: true,
+	},
+);
+
+module.exports = model('Anime', AnimeSchema);
