610f74a99c6e5f04f015e1fec61c84f522d44e4d
rearalf <rear_recinos@hotmail.com>
2020-11-26 14:54:22 -0600

    Agrega animes a la base

--- src/controllers/anime.controller.js
+++ src/controllers/anime.controller.js
@@ -2,8 +2,24 @@ const animeCtrl = {};
 const Anime = require('../models/Anime');
 
 animeCtrl.getAllAnimes = async (req, res) => {
-	const AllAnimes = await Anime.find();
+	const { id_user } = req.body;
+	const AllAnimes = await Anime.find({ id_user });
 	res.json({ message: 'Animes', AllAnimes });
+};
+
+animeCtrl.addAnime = async (req, res) => {
+	const { id_user, mal_id, image_url, title, type, genres, watched, favorite } = req.body;
+	const validateAnime = await Anime.findOne({ mal_id });
+	if (validateAnime) return res.status(401).json({ message: 'Anime already added has seen' });
+	const anime = Anime({ id_user, mal_id, image_url, title, type, genres, watched, favorite });
+	await anime
+		.save()
+		.then(data => {
+			res.json({ message: 'Added anime', data });
+		})
+		.catch(error => {
+			res.json({ message: 'Error adding anime', error });
+		});
 };
 
 module.exports = animeCtrl;
--- src/routes/anime.routes.js
+++ src/routes/anime.routes.js
@@ -1,11 +1,11 @@
 const router = require('express').Router();
-const { getAllAnimes } = require('../controllers/anime.controller');
+const { getAllAnimes, addAnime } = require('../controllers/anime.controller');
 const Auth = require('../middleware/Auth');
 
 router.get('/test', (req, res) => {
 	res.json({ message: 'Anime path' });
 });
 
-router.route('/').all(Auth).get(getAllAnimes);
+router.route('/').all(Auth).get(getAllAnimes).post(addAnime);
 
 module.exports = router;
